953cebd41b241e50e8646550454cc5080a0e8db4
IbrahimMSabek <ibrahim.sabek.85@gmail.com>
2018-04-17 12:56:53 +0200

    Add parameters as JSON

--- src/main/webapp/js/login/controllers/loginController.js
+++ src/main/webapp/js/login/controllers/loginController.js
@@ -5,21 +5,43 @@ Login.controller('loginController',function($scope,$state,$ionicLoading,loginUti
     $scope.init = function(){
        $scope.parameters = [];
        $scope.securityCheckName = "";
+
+       $scope.isJsonParameters = false;
+
+       $scope.data = {};
+       $scope.data.parametersJson = "";
+
+       $scope.isParamsSelected = false;
     };
 
     $scope.addParameter = function(){
         $scope.parameters.push({key:"",value:""});
+        $scope.isParamsSelected = true;
+    }
+
+    $scope.addJsonParameter = function(){
+        $scope.isJsonParameters = true;
     }
 
     $scope.login = function(){
-        if($scope.parameters.length == 0){
-            return;
-        }
+
         var loginParameters = {};
-        for(var i=0;i<$scope.parameters.length;i++){
-            if($scope.parameters[i].key != "")
-                loginParameters[$scope.parameters[i].key] = $scope.parameters[i].key == "password" ? compute($scope.parameters[i].value) : $scope.parameters[i].value;
+
+        if($scope.parameters.length == 0 && $scope.data.parametersJson == ""){
+            return;
+        }else if($scope.isJsonParameters){
+            loginParameters = JSON.parse($scope.data.parametersJson);
+            if("password" in loginParameters){
+                loginParameters.password = compute(loginParameters.password);
+            }
+        }else{
+            for(var i=0;i<$scope.parameters.length;i++){
+                if($scope.parameters[i].key != "")
+                    loginParameters[$scope.parameters[i].key] = $scope.parameters[i].key == "password" ? compute($scope.parameters[i].value) : $scope.parameters[i].value;
+            }
         }
+        
+        
         console.log(loginParameters);
         console.log($scope.securityCheckName);
 
--- src/main/webapp/js/requests/controllers/addRequestController.js
+++ src/main/webapp/js/requests/controllers/addRequestController.js
@@ -5,23 +5,39 @@ Requests.controller('addRequestController',function($scope,$state,$ionicLoading,
     $scope.init = function(){
         if ($state.params.index != null & $state.params.index != undefined) {
             var requestsCollection = localStorage.getItem("requests") != null ? JSON.parse(localStorage.getItem("requests")) : [];
-            $scope.parameters = requestsCollection[$state.params.index].parameters;
+            //$scope.parameters = requestsCollection[$state.params.index].parameters;
             $scope.adapterName = requestsCollection[$state.params.index].adapterName;
             $scope.procedureName = requestsCollection[$state.params.index].procedureName;
             $scope.methodType = requestsCollection[$state.params.index].methodType;
             $scope.consumeType = requestsCollection[$state.params.index].consumeType;
+
+            $scope.isJsonParameters = true;
+            $scope.data = {};
+            $scope.data.parametersJson = JSON.stringify(requestsCollection[$state.params.index].parameters);
         }else{
             $scope.parameters = [];
             $scope.adapterName = "";
             $scope.procedureName = "";
             $scope.methodType = "";
             $scope.consumeType = "";
+
+            $scope.isJsonParameters = false;
+            
+            $scope.data = {};
+            $scope.data.parametersJson = "";
+            
+            $scope.isParamsSelected = false;
         }
         
     };
 
     $scope.addParameter = function(){
         $scope.parameters.push({key:"",value:""});
+        $scope.isParamsSelected = true;
+    }
+
+    $scope.addJsonParameter = function(){
+        $scope.isJsonParameters = true;
     }
 
     $scope.save = function(){
@@ -35,10 +51,16 @@ Requests.controller('addRequestController',function($scope,$state,$ionicLoading,
             "consumeType" : "",
             "parameters" : {}
         };
-        for(var i=0;i<$scope.parameters.length;i++){
-            if($scope.parameters[i].key != "")
-                requestParameters.parameters[$scope.parameters[i].key] = $scope.parameters[i].key == "password" ? compute($scope.parameters[i].value) : $scope.parameters[i].value;
+
+        if($scope.isJsonParameters){
+            requestParameters.parameters = JSON.parse($scope.data.parametersJson);
+        }else{
+            for(var i=0;i<$scope.parameters.length;i++){
+                if($scope.parameters[i].key != "")
+                    requestParameters.parameters[$scope.parameters[i].key] = $scope.parameters[i].key == "password" ? compute($scope.parameters[i].value) : $scope.parameters[i].value;
+            }
         }
+        
         console.log(requestParameters);
 
         if(requestParameters.length == 0 || $scope.adapterName == "" || $scope.procedureName == "" || $scope.methodType == ""){
